11c4f042747d3f9f7629816058f4e1f9fa8a3bf8
Sherry <52001212+sherry-lxy@users.noreply.github.com>
2019-10-03 22:06:18 +0900

    マインスイーパー(Python)

--- minesweeper.py
+++ minesweeper.py
@@ -1,4 +1,6 @@
 import random
+from itertools import product # 多重ループを減らす
+import numpy as np
 
 MS_SIZE = 8          # ゲームボードのサイズ
 CLOSE, OPEN, FLAG = 0, 1, 2
@@ -24,18 +26,7 @@ class Game:
     def init_game_board(self):
         """ ゲーム盤を初期化 """
 
-        self.game_board = [[CLOSE for i in range(MS_SIZE)] for j in range(MS_SIZE)]
-
-        '''
-        self.game_board = [[0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0]]
-        '''                   
+        self.game_board = np.array([[CLOSE for i in range(MS_SIZE)] for j in range(MS_SIZE)])              
 
     def init_mine_map(self, number_of_mines):
         """ 地雷マップ(self->mine_map)の初期化
@@ -45,41 +36,44 @@ class Game:
         地雷セルに-1を設定する．      
         """
 
-        self.mine_map = [[CLOSE for i in range(MS_SIZE)] for j in range(MS_SIZE)]
+        self.mine_map = np.array([[CLOSE for i in range(MS_SIZE)] for j in range(MS_SIZE)])
+
+        # 例外処理
         if number_of_mines < 0:
         	number_of_mines = 0
         elif number_of_mines > 64:
         	number_of_mines = 64
-        
+
+
+        fy = np.array([i for i in range(MS_SIZE**2)]) # 0~63の行列の用意する
+        end = MS_SIZE ** 2 # 最後位
 
         for i in range(number_of_mines):
-        	x = random.randrange(MS_SIZE)
-       		y = random.randrange(MS_SIZE)
-       		while self.mine_map[y][x] == -1:
-       			x = random.randrange(MS_SIZE)
-       			y = random.randrange(MS_SIZE)
-       		self.mine_map[y][x] = -1
-     
+        	m = random.choice(range(0,end)) # 0~end-1までランダムに数値を取り出す
+        	y = int(fy[m] / MS_SIZE) # それに対応する2次元配列の座標に変換する
+        	x = fy[m] % MS_SIZE
+        	self.mine_map[y][x] = -1 # 地雷として設定する
+        	fy[m] = fy[end - 1] # 一番後ろの数値ともうすでに取り出した数値と書き換える
+        	end -= 1 # 最後位を更新する
+
     def count_mines(self):
         """ 8近傍の地雷数をカウントしmine_mapに格納 
         地雷数をmine_map[][]に設定する．
         """
-
+		
         for y in range(MS_SIZE):
         	for x in range(MS_SIZE):
         		if self.mine_map[y][x] == -1:
         			pass
-        		else:	
-        			for j in range(-1, 2):
-        				for i in range(-1, 2):
-        					if y+j<0 or x+i<0 or y+j>7 or x+i>7: # 例外処理
-        						pass
-        					else:
-        						if self.mine_map[y + j][x + i] == -1:
-        							self.mine_map[y][x] += 1
+        		else:
+        			# 例外処理
+        			y_lower = 0 if y-1<0 else y-1
+        			y_upper = MS_SIZE if y+2>MS_SIZE else y+2
+        			x_lower = 0 if x-1<0 else x-1
+        			x_upper = MS_SIZE if x+2>MS_SIZE else x+2
 
+        			self.mine_map[y][x] = np.sum(self.mine_map[y_lower:y_upper, x_lower:x_upper] == -1) # 地雷数をカウントする
 
-    
     def open_cell(self, x, y):
         """ セル(x, y)を開ける
         Arguments:
@@ -91,33 +85,34 @@ class Game:
                    地雷セル，FLAGが設定されたセルは開けない．
           False -- 地雷があるセルを開けてしまった場合（ゲームオーバ）
         """
-        
-        if self.mine_map[y][x] == -1:
+
+        row = [-1, 0, 1]
+  
+        if self.mine_map[y][x] == -1: # 地雷を選んだ場合ゲーム終了
        		return False
-       	elif self.game_board[y][x] == OPEN:
+       	elif self.game_board[y][x] == OPEN: # もうすでに開けてるセルを選んだ場合何も処理しない
        		pass
        		return True
-       	elif self.game_board[y][x] == FLAG:
+       	elif self.game_board[y][x] == FLAG: # flagのところを選んだらセルを開ける状態に切り替える
        		self.game_board[y][x] = OPEN
-       	elif self.game_board[y][x] == CLOSE:
+       	elif self.game_board[y][x] == CLOSE: # closeのセルを開ける
        		self.game_board[y][x] = OPEN
        	
-       	for j in range(-1, 2):
-       		for  i in range(-1, 2):
-       			if y+j<0 or x+i<0 or y+j>7 or x+i>7:
+       	for j, i in product(row, row):
+       		if y+j<0 or x+i<0 or y+j>7 or x+i>7: # 例外処理
+       			pass
+       		elif i == 0 and j == 0:
+       			pass
+       		else:
+        		if self.game_board[y + j][x + i] == OPEN: # 既に開いているセルの近傍セルは開けない
+        			pass
+        		elif self.game_board[y + j][x + i] == FLAG:	# FLAGが設定されたセルは開けない
        				pass
-       			elif i == 0 and j == 0:
-       				pass
-       			else:
-        			if self.game_board[y + j][x + i] == OPEN:
-        				pass
-        			elif self.game_board[y + j][x + i] == FLAG:	
+       			elif self.game_board[y + j][x + i] == CLOSE:
+       				if self.mine_map[y + j][x + i] == -1: # 地雷セルは開けない
        					pass
-       				elif self.game_board[y + j][x + i] == CLOSE:
-       					if self.mine_map[y + j][x + i] == -1:
-       						pass
-       					elif self.mine_map[y + j][x + i] >= 0:
-       						self.game_board[y + j][x + i] = OPEN	
+       				elif self.mine_map[y + j][x + i] >= 0:
+       					self.game_board[y + j][x + i] = OPEN	
 
         return True
     
@@ -135,23 +130,13 @@ class Game:
             
     def is_finished(self):
         """ 地雷セル以外のすべてのセルが開かれたかチェック """
+		
+        for y, x in product(range(MS_SIZE), range(MS_SIZE)):
+        	if not((self.game_board[y][x] != OPEN and self.mine_map[y][x] == -1) or (self.game_board[y][x] == OPEN and self.mine_map[y][x] >= 0)):
+        		return False
 
-        count = 0
-
-        for y in range(MS_SIZE):
-        	for x in range(MS_SIZE):
-        		if self.game_board[y][x] != OPEN and self.mine_map[y][x] == -1:
-        			count += 1
-        		elif self.game_board[y][x] == OPEN and self.mine_map[y][x] >= 0:
-        			count += 1
-
-        if count == MS_SIZE ** 2:
-        	return True
-        else:
-        	return False
-
-
-        
+        return True
+       
     def print_header(self):
         print("=====================================")
         print("===  Mine Sweeper Python Ver. 1  ====")
